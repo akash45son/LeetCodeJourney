1e236991cf5399ca810cf788418a829d4fe1c12c
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-06-23 00:46:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (64.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,10 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
      int start = 0;
-     int end = n-1;  
-     int sum; 
+     int end = n-1;   
      while(start<end){
-        sum = nums[start] + nums[end];
+        int sum = nums[start] + nums[end];
         if(sum == target ){
             return {start+1,end+1};
         }
