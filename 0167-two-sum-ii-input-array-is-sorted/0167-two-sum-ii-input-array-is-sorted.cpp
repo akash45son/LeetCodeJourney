c11bb5981ac29ff43118d64f4883b4c9bd2fd2e6
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-06-23 00:45:06 +0530

    Time: 0 ms (100%), Space: 19.4 MB (64.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -5,7 +5,7 @@ public:
      int start = 0;
      int end = n-1;  
      int sum; 
-     while(start<=end){
+     while(start<end){
         sum = nums[start] + nums[end];
         if(sum == target ){
             return {start+1,end+1};
