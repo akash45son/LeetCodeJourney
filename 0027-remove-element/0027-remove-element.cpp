0578171b885f3e9fdaa4c757b4682e0633996b51
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-06-27 01:04:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (99.27%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int start = 0;
+     for(int i=0;i<nums.size();i++){
+        if(nums[i]!=val){
+            nums[start]=nums[i];
+            start++;
+        }
+
+     }  
+     return start;;
+    }
+};
