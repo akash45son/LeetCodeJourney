18099b0033211bc20acf68a469950e0065094c74
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-07-11 00:35:51 +0530

    Time: 3 ms (41.26%), Space: 9.3 MB (82.13%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }else{
+            unordered_map<char,char> mp1;
+            unordered_map<char,char> mp2;
+
+            for(int i=0; i<s.size(); i++){
+                if(mp1.count(s[i])==0 && mp2.count(t[i])==0){
+                    mp1[s[i]]=t[i];
+                    mp2[t[i]]=s[i];
+                }else{
+                    if(mp1[s[i]]!=t[i] || mp2[t[i]]!=s[i]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
