b80afaade94a0cba94260209e605b31b9391f6b8
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-07-10 22:28:55 +0530

    Time: 0 ms (100%), Space: 11.8 MB (58.14%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        string ans = "";
+        for(int i=0; i<strs[0].size();i++){
+           char ch = strs[0][i];
+            for(int j=1;j<n;j++){
+                if(i>=strs[j].size()){
+                    return ans;
+                }
+                if(strs[j][i]!=ch){
+                    return ans;
+                }
+               }
+                    ans += ch;
+               
+            }
+    return ans;
+    }
+};
