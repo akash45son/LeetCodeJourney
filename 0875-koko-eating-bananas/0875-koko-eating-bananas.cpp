35b2e485f6c38e08c42b3e1d623967882e6f5f32
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-07-14 00:31:57 +0530

    Time: 7 ms (70.48%), Space: 23.1 MB (14.81%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -5,6 +5,7 @@ public:
       int maximum = *max_element(piles.begin(), piles.end());
       int start = 1;
       int end = maximum;
+      int ans ;
     while(start<=end){
         int mid = start+ (end-start)/2;
         long long hours = 0;
@@ -12,12 +13,13 @@ public:
             hours += (piles[j]+mid-1)/mid;
         }
         if(hours<=h){
+            ans = mid;
             end = mid-1;
         }
         else{
             start = mid+1;
         }
     } 
-    return start;
+    return ans;
     }
 };
