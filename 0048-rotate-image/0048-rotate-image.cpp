1174f8a720ca161225595f0bfaefb253214ea494
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-07-17 00:20:00 +0530

    Time: 0 ms (100%), Space: 10.5 MB (9.52%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> ans(n,vector<int>(m));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[j][n-1-i]=matrix[i][j];
+            }
+        }
+        matrix=ans;
+    }
+};
