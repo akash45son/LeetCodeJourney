91e05f7a007bbbee956a5e2253e7248be1f4e7ce
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-07-19 11:41:31 +0530

    Time: 1 ms (17.24%), Space: 9.2 MB (7.4%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        unordered_map<char,int> mp;
+        unordered_set<char> st;
          if(sentence.size()<26){
             return false;
          }
         else{
-        for(char ch='a';ch<='z';ch++){
-            mp[ch]++;
-        }
         for(int i=0;i<sentence.size();i++){
-            mp[sentence[i]]--;
+            st.insert(sentence[i]);
             
         }
-        for(auto x : mp){
-            if(x.second>0){
-                return false;
-            }
-        }
+       
          }
-        return true;
+        return st.size()==26;
     }
 };
