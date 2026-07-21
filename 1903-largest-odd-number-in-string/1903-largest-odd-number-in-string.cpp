a9ca195a82ad0cf557312c79d51d62627487054b
AKASH DNYANESHWAR SONAWANE <1985swatid@gmail.com>
2026-07-21 10:55:08 +0530

    Time: 0 ms (100%), Space: 16.8 MB (95.67%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         int largest = INT_MIN;
-        string ans ="";
+        
     for(int i=num.size()-1;i>=0;i--){
         if((num[i]-'0')%2==1){
             
@@ -11,6 +11,6 @@ public:
             num.erase(i);
         }
     }
-    return ans;
+    return "";
     }
 };
